98768cbf1933f1b0d4822211948591ed447342e3
muffins <muffins@gmail.com>
2025-05-18 20:08:22 -0400

    time fix

--- util.js
+++ util.js
@@ -1,10 +1,9 @@
 export const trimInput = (input) => {
-    return input.trim().replace(/\s/g, "")
+  return input.trim().replace(/\s/g, '')
 }
 
 export const getApiDate = () => {
-  const date = new Date();
-  const formattedDate = date.toISOString().slice(0, 10);
-  // const formattedDate = "2025-05-12";
-  return formattedDate;
-};
+  const date = new Date()
+  const formattedDate = date.toLocaleDateString('en-US')
+  return formattedDate
+}
