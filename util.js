db5bbb39d37bc9d39e772c5c244914d2671022dd
muffins <muffins@gmail.com>
2025-05-18 20:14:28 -0400

    date format fix

--- util.js
+++ util.js
@@ -5,6 +5,8 @@ export const trimInput = (input) => {
 export const getApiDate = () => {
   const date = new Date()
   date.setDate(date.getDate() - 2)
-  const formattedDate = date.toLocaleDateString('en-US')
-  return formattedDate
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
 }
