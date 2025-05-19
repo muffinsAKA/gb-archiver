6f94b36107ed9f640a272651fb217e986054e46b
muffins <muffins@gmail.com>
2025-05-18 20:11:38 -0400

    add 2 day delay

--- util.js
+++ util.js
@@ -4,6 +4,7 @@ export const trimInput = (input) => {
 
 export const getApiDate = () => {
   const date = new Date()
+  date.setDate(date.getDate() - 2)
   const formattedDate = date.toLocaleDateString('en-US')
   return formattedDate
 }
